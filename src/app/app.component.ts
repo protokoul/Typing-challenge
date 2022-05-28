459eba29c8d7dbcd269e099164b6b1e07aac0bc4
anubhav <anubhavkoul01@gmail.com>
2022-05-28 23:30:43 +0530

    Typing challenge using Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  userInput = '';
+  successMessage = false;
+
+  onChangeInput(value: string) {
+    this.userInput = value;
+    this.successMessage = this.randomText === this.userInput;
+  }
+
+  compare(randomLetter: string, enteredLetter: string){
+    if(!enteredLetter){
+      return 'pending';
+    }
+  
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
+
 }
